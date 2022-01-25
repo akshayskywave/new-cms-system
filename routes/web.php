1e54bd502f5a619697da6fd7923683b5376720f6
akshayskywave <98301899+akshayskywave@users.noreply.github.com>
2022-01-25 19:06:39 +0530

    post show done

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
+Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');
