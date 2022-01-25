b4f2259672d5e8bf733f98c7464c531f94288f1d
akshayskywave <98301899+akshayskywave@users.noreply.github.com>
2022-01-25 18:27:46 +0530

    migration

--- resources/views/components/home-master.blade.php
+++ resources/views/components/home-master.blade.php
@@ -34,6 +34,17 @@
               <span class="sr-only">(current)</span>
             </a>
           </li>
+
+          @if(Auth::check())
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('admin.index')}}">Admin</a>
+          </li>
+          @else
+          <li class="nav-item">
+            <a class="nav-link" href="/Login">Login</a>
+          </li>
+          @endif
+        
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
           </li>
